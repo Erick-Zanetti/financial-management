2fd7b68b0ae89f5ff635965b02c4e38a93e831a3
Erick Zanetti <erick_98zanetti_98@hotmail.com>
2024-04-11 14:22:40 -0300

    backup by Java

--- server/src/main/java/financial/management/com/server/api/ServerApplication.java
+++ server/src/main/java/financial/management/com/server/api/ServerApplication.java
@@ -4,10 +4,12 @@ import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableMongoRepositories
 @EnableRabbit
 @SpringBootApplication
+@EnableScheduling
 public class ServerApplication {
 
 	public static void main(String[] args) {
